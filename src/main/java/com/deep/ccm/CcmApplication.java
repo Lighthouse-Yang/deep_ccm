b4c2d1475df69da97d09165e5c6c9ec4cc74fea1
Iamyangbo <807490539@qq.com>
2018-12-13 14:43:39 +0800

    first

---
+++ src/main/java/com/deep/ccm/CcmApplication.java
@@ -0,0 +1,14 @@
+package com.deep.ccm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcmApplication.class, args);
+	}
+
+}
+
